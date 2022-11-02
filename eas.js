ae770af43a0b9a227398fbcdea919c8b60530bc3
bilel <bilel.moudache0412@gmail.com>
2022-11-02 17:54:13 +0100

    finish the addEvent listen on sketch
    next add addEvent litenr to contole button
    add buton reset

---
+++ eas.js
@@ -0,0 +1,32 @@
+const sketch = document.querySelector('div#sketch');
+let numberCase = 0;
+
+numberCase = prompt('Combiens de case veux tu ?');
+
+function doWidthDiv(number){
+    let y = 900/number
+    
+    return Math.sqrt(y);
+}
+
+function dispalyDiv(numberCases){
+    const div = document.createElement('div');
+    for( i=0 ; i < numberCases ; i++){
+        const div = document.createElement('div');
+        div.setAttribute('id', 'square')
+        div.style.cssText = `width:${doWidthDiv(numberCase)}rem ;height:${doWidthDiv(numberCase)}rem `
+        sketch.appendChild(div)
+    }
+}
+
+
+dispalyDiv(numberCase);
+
+const divs = document.querySelectorAll('div#square')
+
+
+    divs.forEach(div => {
+        div.addEventListener('mouseover',function(){
+        div.style.cssText = `background-color : black ;width:${doWidthDiv(numberCase)}rem ;height:${doWidthDiv(numberCase)}rem `
+        });
+})
